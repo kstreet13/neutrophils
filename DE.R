ea51e815f3dbff36632b6fff98c76db5b7f8d38d
Kelly Street <street.kelly@gmail.com>
2024-01-31 11:58:07 -0800

    made writeup, PCA.qmd

--- DE.R
+++ DE.R
@@ -13,3 +13,41 @@ res <- DESeq(ds)
 
 tab <- results(res, name=resultsNames(res)[3])
 rownames(tab)[which(tab$padj < .01)]
+
+
+
+
+
+####
+
+# find genes DE between any pair of labels
+
+pheno$color <- factor(pheno$color)
+
+de <- NULL
+for(c in unique(pheno$color)){
+    pheno$color <- relevel(pheno$color, ref = c)
+    
+    ds <- DESeqDataSetFromMatrix(countData = counts,
+                                 colData = pheno,
+                                 design= ~ color)
+    res <- DESeq(ds)
+    
+    for(n in resultsNames(res)[-1]){
+        tab <- results(res, name=n)
+        de <- unique(c(de, rownames(tab)[which(tab$padj < .01)]))
+    }
+}
+
+
+
+zscore <- cpm[de,]
+zscore <- (zscore - rowMeans(zscore)) / rowSds(zscore)
+png(filename = '~/Desktop/heat.png', width = 1500, height = 2000)
+heatmap(as.matrix(zscore), ColSideColors = as.character(pheno$color))
+dev.off()
+# not much structure
+
+
+
+
--- pca.R
+++ pca.R
@@ -10,9 +10,10 @@ plotRLE(as.matrix(counts), col = pheno$color)
 plotRLE(as.matrix(cpm), col = pheno$color)
 
 # quick PCA
-cpm <- cpm[which(rowMeans(cpm) > 5), ]
+filt <- which(rowMeans(cpm) > 1)
+#filt <- which(rowVars(cpm) >= sort(rowVars(cpm), decreasing = TRUE)[10000])
 
-pca <- BiocSingular::runPCA(t(log1p(cpm)), rank=64)
+pca <- BiocSingular::runPCA(t(log1p(cpm[filt, ])), rank=64)
 
 plot(pca$x, asp=1, col=pheno$color, cex=3)
 
@@ -23,9 +24,28 @@ text(pca$x, labels = pheno$patient, col=pheno$color)
 
 require(plotly)
 
-
 plot_ly(data.frame(pca$x), x=~PC1, y=~PC2, z=~PC3, color = pheno$color, colors = sort(unique(pheno$color)))
 
 
+# PCA on subsets:
+# 1. Surgical controls vs healthy controls (all timepoints)
+ind <- which(pheno$group %in% c('SC','EC'))
+
+subcpm <- cpm[,ind]
+filt <- which(rowMeans(subcpm) > 1)
+pca <- BiocSingular::runPCA(t(log1p(subcpm[filt, ])), rank=length(ind))
+plot(pca$sdev^2/sum(pca$sdev^2))
+plot_ly(data.frame(pca$x), x=~PC1, y=~PC2, z=~PC3, color = pheno$color[ind], colors = sort(unique(pheno$color[ind])))
+
+
+
+# 2. Post-bypass timepoints (B and C) all patients ?
+ind <- which(pheno$timepoint %in% c('B','C'))
+
+subcpm <- cpm[,ind]
+filt <- which(rowMeans(subcpm) > 1)
+pca <- BiocSingular::runPCA(t(log1p(subcpm[filt, ])), rank=length(ind))
+plot(pca$sdev^2/sum(pca$sdev^2))
+plot_ly(data.frame(pca$x), x=~PC1, y=~PC2, z=~PC3, color = pheno$color[ind], colors = sort(unique(pheno$color[ind])))
 
 
