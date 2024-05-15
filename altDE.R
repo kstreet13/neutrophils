596295d35b3e4eaf7533b5f711aab3e599dfac85
Kelly Street <street.kelly@gmail.com>
2024-05-15 16:52:56 -0700

    fixed altDE, improved plots

--- altDE.R
+++ altDE.R
@@ -21,33 +21,33 @@
     names(res2) <- paste0('AC_', names(res2))
     names(res3) <- paste0('BC_', names(res3))
     
-    hits <- cbind(res1,res2,res3)
-    hits <- hits[which((hits$AB_padj < .05 & abs(hits$AB_log2FoldChange) > 1.5) | 
-                           (hits$AC_padj < .05 & abs(hits$AC_log2FoldChange) > 1.5) | 
-                           (hits$BC_padj < .05 & abs(hits$BC_log2FoldChange) > 1.5)), ]
-    
+    deseq <- cbind(res1,res2,res3)
+
     rm(res,res1,res2,res3,ind,ds)
 } # hits
 
 source('setup.R')
 
 counts <- counts[, pheno$group == 'SC']
+
+cpm <- t(t(1e6*counts)/colSums(counts))
+
 pheno <- pheno[pheno$group == 'SC', ]
 # check order
 all(pheno$sample == c("SC1A","SC2A","SC3A","SC4A","SC5A","SC6A","SC7A","SC8A","SC1B","SC2B","SC3B","SC4B","SC5B","SC6B","SC7B","SC8B","SC1C","SC2C","SC3C","SC4C","SC5C","SC6C","SC7C","SC8C"))
 all(colnames(counts) == c("SC1A","SC2A","SC3A","SC4A","SC5A","SC6A","SC7A","SC8A","SC1B","SC2B","SC3B","SC4B","SC5B","SC6B","SC7B","SC8B","SC1C","SC2C","SC3C","SC4C","SC5C","SC6C","SC7C","SC8C"))
 
-A <- counts[,1:8]
-B <- counts[,9:16]
-C <- counts[,17:24]
+A <- cpm[,1:8]
+B <- cpm[,9:16]
+C <- cpm[,17:24]
 
-AB <- A/B; AB[is.nan(AB)] <- 1 # 1/0 = Inf, 0/0 = NaN
-AC <- A/C; AC[is.nan(AC)] <- 1
-BC <- B/C; BC[is.nan(BC)] <- 1
+AB <- B/A; AB[is.nan(AB)] <- 1 # 1/0 = Inf, 0/0 = NaN
+AC <- C/A; AC[is.nan(AC)] <- 1
+BC <- C/B; BC[is.nan(BC)] <- 1
 
 
 require(matrixStats)
-res <- data.frame(
+clfc <- data.frame(
     ABup = rowMins(as.matrix(AB)) > 1.5,
     ABdn = rowMaxs(as.matrix(AB)) < 2/3,
     ACup = rowMins(as.matrix(AC)) > 1.5,
@@ -55,22 +55,51 @@ res <- data.frame(
     BCup = rowMins(as.matrix(BC)) > 1.5,
     BCdn = rowMaxs(as.matrix(BC)) < 2/3
 )
+rownames(clfc) <- rownames(cpm)
+
+rm(A,B,C,AB,BC,AC)
+
+
+table(deseq$AB_padj < .05 & abs(deseq$AB_log2FoldChange) > 1.5, clfc$ABup | clfc$ABdn, useNA = 'ifany')
+
+table(deseq$AC_padj < .05 & abs(deseq$AC_log2FoldChange) > 1.5, clfc$ACup | clfc$ACdn, useNA = 'ifany')
+
+table(deseq$BC_padj < .05 & abs(deseq$BC_log2FoldChange) > 1.5, clfc$BCup | clfc$BCdn, useNA = 'ifany')
 
 
 
-ABhits.DESeq <- rownames(hits)[hits$AB_padj < .05]
-ABhits.FC <- rownames(counts)[which(res$ABup | res$ABdn)]
-x <- unique(c(ABhits.DESeq, ABhits.FC))
-table(x %in% ABhits.DESeq, x %in% ABhits.FC)
+
+plot(deseq$AB_log2FoldChange, -log10(deseq$AB_padj), col='grey', main = 'Timepoint A vs. B', xlab='Avg Log2 FC', ylab='-log10 Padj', pch=16)
+ind <- which(deseq$AB_padj < .05 & abs(deseq$AB_log2FoldChange) > 1.5)
+points(deseq$AB_log2FoldChange[ind], -log10(deseq$AB_padj)[ind], col=4, pch=16)
+ind <- which(clfc$ABup)
+points(deseq$AB_log2FoldChange[ind], -log10(deseq$AB_padj)[ind], col=3, cex = .75, pch=16)
+ind <- which(clfc$ABdn)
+points(deseq$AB_log2FoldChange[ind], -log10(deseq$AB_padj)[ind], col=2, cex = .75, pch=16)
+abline(v = c(-1.5,1.5), col=4); abline(h = -log10(.05), col=4)
+legend('topleft',bty='n',cex=.75,pch=16, col = c(4,3,2), legend = c('DESeq2 deseq', 'all LFC >1.5', 'all LFC <-1.5'))
 
 
-plot(hits$AB_log2FoldChange, -log10(hits$AB_padj))
-abline(v = c(-1.5,1.5)); abline(h = -log10(.05))
-ind <- which(rownames(hits) %in% ABhits.FC)
-points(hits$AB_log2FoldChange[ind], -log10(hits$AB_padj)[ind], col=2)
+plot(deseq$AC_log2FoldChange, -log10(deseq$AC_padj), col='grey', main = 'Timepoint A vs. C', xlab='Avg Log2 FC', ylab='-log10 Padj', pch=16)
+ind <- which(deseq$AC_padj < .05 & abs(deseq$AC_log2FoldChange) > 1.5)
+points(deseq$AC_log2FoldChange[ind], -log10(deseq$AC_padj)[ind], col=4, pch=16)
+ind <- which(clfc$ACup)
+points(deseq$AC_log2FoldChange[ind], -log10(deseq$AC_padj)[ind], col=3, cex = .75, pch=16)
+ind <- which(clfc$ACdn)
+points(deseq$AC_log2FoldChange[ind], -log10(deseq$AC_padj)[ind], col=2, cex = .75, pch=16)
+abline(v = c(-1.5,1.5), col=4); abline(h = -log10(.05), col=4)
+legend('topleft',bty='n',cex=.75,pch=16, col = c(4,3,2), legend = c('DESeq2 deseq', 'all LFC >1.5', 'all LFC <-1.5'))
 
 
-
+plot(deseq$BC_log2FoldChange, -log10(deseq$BC_padj), col='grey', main = 'Timepoint B vs. C', xlab='Avg Log2 FC', ylab='-log10 Padj', pch=16)
+ind <- which(deseq$BC_padj < .05 & abs(deseq$BC_log2FoldChange) > 1.5)
+points(deseq$BC_log2FoldChange[ind], -log10(deseq$BC_padj)[ind], col=4, pch=16)
+ind <- which(clfc$BCup)
+points(deseq$BC_log2FoldChange[ind], -log10(deseq$BC_padj)[ind], col=3, cex = .75, pch=16)
+ind <- which(clfc$BCdn)
+points(deseq$BC_log2FoldChange[ind], -log10(deseq$BC_padj)[ind], col=2, cex = .75, pch=16)
+abline(v = c(-1.5,1.5), col=4); abline(h = -log10(.05), col=4)
+legend('topleft',bty='n',cex=.75,pch=16, col = c(4,3,2), legend = c('DESeq2 deseq', 'all LFC >1.5', 'all LFC <-1.5'))
 
 
 
