451a3e00efe319927c983dc0efb2d59844c1e8b2
Kelly Street <street.kelly@gmail.com>
2024-08-27 17:15:39 -0700

    sc figures

--- figures_SC.R
+++ figures_SC.R
@@ -18,13 +18,14 @@ subpheno <- pheno[match(colnames(heatdat), pheno$sample), ]
 require(NMF)
 anncolors = list(
     timepoint = c(A = "green", B = "red", C = "purple"))
-aheatmap(log1p(heatdat), annCol = subpheno[,"timepoint",drop=FALSE],
+aheatmap(log1p(heatdat), 
+         annCol = subpheno[,"timepoint",drop=FALSE],
          Colv = NA, scale = "row",
          color = "RdBu:100",
          distfun = "pearson",
          annColors = anncolors,
          filename = '~/Desktop/heat.png')
-# Normalized expression values are calculated by log(CPM+1). For the heatmap, each gene is then normalized to a mean of 0 and standard deviation of 1 to show differences between timepoints.
+# Expression values are normalized by dividing each count by the total for that sample and multiplying by 1 million (Counts Per Million, CPM), then adding a pseudocount of 1 and taking the natural log, to control outliers. For the heatmap, each gene's normalized expression is then standardized to a mean of 0 and standard deviation of 1 to show differences between timepoints.
 
 
 
@@ -89,30 +90,112 @@ for(i in 1:nrow(means)){
 # 1) all genes, 2) DE genes
 
 x <- cbind(deseq$AB_log2FoldChange, deseq$AC_log2FoldChange)
-ind <- !is.na(x[,1])
-x <- x[ind, ]
-x <- apply(x,1,function(x){
-    x[which.max(abs(x))]
+ind <- ifelse(abs(x[,2]) > abs(x[,1]), 2, 1)
+ind[is.na(ind)] <- 1
+x <- sapply(1:nrow(x),function(i){
+    x[i, ind[i]]
 })
+
 means <- t(apply(log1p(cpm),1,function(cts){
     c(mean(cts[1:8]), mean(cts[9:16]), mean(cts[17:24]))
 }))
-y <- means[ind,1]
+y <- means[,1]
 
+# all genes
+plot(x,y, col=rgb(0,0,0,.5),
+     xlab = 'Max. (abs) fold change from baseline',
+     ylab = 'Avg. normalized expression at baseline',
+     main = 'All genes')
+abline(v=0,lty=2)
 
-plot(x,y)
+# de genes
+DEind <- which(deseq$AB_padj < .05 | deseq$AC_padj < .05 | deseq$BC_padj < .05)
+plot(x[DEind], y[DEind], col=rgb(0,0,0,.5),
+     xlab = 'Max. (abs) fold change from baseline',
+     ylab = 'Avg. normalized expression at baseline',
+     main = 'DE genes')
+abline(v=0,lty=2)
 
+hist(y[x < 0])
+hist(y[x > 0])
 
-
-
+############
+# PATTERNS #
+############
 # define recovery:
 # DE in A->B, not DE in A->C and B->C shift has opposite sign
-#  DE/not DE vs. A-B, A-C, B-C
+#recov.ind <- which(deseq$AB_padj < .05 & deseq$AC_padj > .05 & (sign(deseq$AB_log2FoldChange) == -sign(deseq$BC_log2FoldChange)))
+recov.ind <- which(deseq$AB_padj < .05 & deseq$BC_padj < .05 & deseq$AC_padj > .05)
 
-table(deseq$AB_padj < .05, deseq$AC_padj < .05)
-
-
-# down and stay down:
+# down early/no recovery:
 # DE in A->B and in A->C, with same sign for both
+#down.ind <- which(deseq$AB_padj < .05 & deseq$AC_padj < .05 & deseq$AB_log2FoldChange < 0  & deseq$AC_log2FoldChange < 0)
+#norecov.ind <- which(deseq$AB_padj < .05 & deseq$AC_padj < .05 & (sign(deseq$AB_log2FoldChange) == sign(deseq$AC_log2FoldChange)))
+early.ind <- which(deseq$AB_padj < .05 & deseq$BC_padj > .05)
+
+# late induction:
+# not DE in A->B, DE in B->C and A->C
+#late.ind <- which(deseq$AB_padj > .05 & deseq$AC_padj < .05 & deseq$BC_padj < .05)
+#late.ind <- which(deseq$AC_padj < .05 & deseq$BC_padj < .05 & (sign(deseq$AC_log2FoldChange) == sign(deseq$BC_log2FoldChange)))
+late.ind <- which(deseq$AB_padj > .05 & deseq$BC_padj < .05)
 
 
+sum(deseq$AB_padj < .05, na.rm=TRUE)
+sum(deseq$AB_padj < .05 & deseq$AC_padj > .05 & (sign(deseq$AB_log2FoldChange) == -sign(deseq$BC_log2FoldChange)), na.rm = TRUE)
+barplot(c(Recovery = 1924, NoRecovery = 4225-1924))
+
+# explainer figure
+plot(c(0,4), c(-2,2), col='white')
+abline(h=0, lty=2)
+lines(c(0,1,0), col=3, lwd=3)
+lines(c(0,-1,0), col=3, lwd=3)
+lines((1:3)-.05, c(0,1,1), col=2, lwd=3)
+lines((1:3)-.05, c(0,-1,-1), col=2, lwd=3)
+lines((1:3), c(0,0,1), col=4, lwd=3)
+lines((1:3), c(0,0,-1), col=4, lwd=3)
+
+
+
+pattern <- rep(NA, nrow(heatdat))
+pattern[rownames(heatdat) %in% rownames(deseq)[recov.ind]] <- 'Recovery'
+pattern[rownames(heatdat) %in% rownames(deseq)[early.ind]] <- 'Early change'
+pattern[rownames(heatdat) %in% rownames(deseq)[late.ind]] <- 'Late change'
+genemeta <- data.frame(Pattern = factor(pattern))
+
+anncolors = list(
+    timepoint = c(A = "green", B = "red", C = "purple"),
+    Pattern = c(`Early change` = 2, Recovery = 3, `Late change` = 4))
+aheatmap(log1p(heatdat), 
+         annCol = subpheno[,"timepoint",drop=FALSE],
+         annRow = genemeta,
+         Colv = NA, scale = "row",
+         color = "RdBu:100",
+         distfun = "pearson",
+         annColors = anncolors,
+         filename = '~/Desktop/heat.png')
+
+
+
+
+# the B->C change tends to reverse the A->B change
+cc <- rep('grey50', nrow(deseq))
+cc[recov.ind] <- 3
+cc[early.ind] <- 2
+cc[late.ind] <- 4
+
+# add counts of different groups
+plot(range(-log10(deseq$AB_padj)*sign(deseq$AB_log2FoldChange), na.rm = TRUE),
+     range(-log10(deseq$BC_padj)*sign(deseq$BC_log2FoldChange), na.rm = TRUE),
+     col = 'white', asp = 1,
+     xlab = 'Signed -log10 p-value for A vs. B',
+     ylab = 'Signed -log10 p-value for B vs. C')
+abline(h=0,lty=2);abline(v=0,lty=2)
+points(-log10(deseq$AB_padj)*sign(deseq$AB_log2FoldChange), -log10(deseq$BC_padj)*sign(deseq$BC_log2FoldChange), col = alpha(cc, alpha=.5), pch=16)
+legend('topright', col = c(2,4,3), pch = 16, bty = 'n', cex = 1,
+       legend = c(paste0('Early change (n=',length(early.ind),')'),
+                  paste0('Late change (n=',length(late.ind),')'),
+                  paste0('Recovery (n=',length(recov.ind),')')))
+
+
+plot(deseq$AB_log2FoldChange, deseq$BC_log2FoldChange, col = alpha(cc, alpha=.5), asp = 1)
+abline(h=0,col=2);abline(v=0,col=2)
